a00ccd336bd184e986641e5eaa821333a7899067
lokesh <devathilokesh2001@gmail.com>
2022-07-10 00:06:49 +0530

    enter

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import {TermsAndConditionsComponent} from './pages/terms-and-conditions/terms-and-conditions.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+
+type NewType = Routes;
+
+const routes: NewType = [
+ { path: '' , component: HomeComponent },
+ { path: 'category', component: SingleCategoryComponent },
+ { path: 'post', component: SinglePostComponent },
+ { path: 'aboutus', component: AboutUsComponent },
+ { path: 'terms-conditions', component: TermsAndConditionsComponent },
+ { path: 'contact', component: ContactUsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
