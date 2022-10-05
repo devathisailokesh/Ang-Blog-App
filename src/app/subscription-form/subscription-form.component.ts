224a24b43f69743280d3f2a681880356bd2df945
lokesh <devathilokesh2001@gmail.com>
2022-10-05 20:14:28 +0530

    commit

--- src/app/subscription-form/subscription-form.component.ts
+++ src/app/subscription-form/subscription-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Sub } from '../models/sub';
+import { SubscribersService } from '../services/subscribers.service';
 
 @Component({
   selector: 'app-subscription-form',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubscriptionFormComponent implements OnInit {
 
-  constructor() { }
+  isEmailError: boolean = false;
+  isSubscribed: boolean = false;
+
+  constructor( private subService: SubscribersService ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit( formVal: any ){
+    
+    const subData: Sub = {
+        name: formVal.name,
+        email: formVal.email
+    }
+
+
+    
+    this.subService.checkSubs(subData.email).subscribe(val => {
+        console.log(val);
+
+        if(val.any){
+          this.subService.addSubs(subData);
+          this.isSubscribed = true;
+        }
+        else{
+         this.isEmailError = true;
+        }
+
+    })
+
   }
 
 }
